eb520fcd8d8f426ea064213b105410594c6de0c0
Andrii Vasyliev <sol@hiqdev.com>
2018-11-28 17:23:07 +0000

    Inited configuration (to be continued)

--- config/common.php
+++ config/common.php
@@ -1,4 +1,12 @@
 <?php
 
 return [
+    'db' => [
+        '__class'   => \yii\db\Connection::class,
+        'dsn'       => 'pgsql:dbname=' . $params['db.name']
+                        . (!empty($params['db.host']) ? (';host=' . $params['db.host']) : '')
+                        . (!empty($params['db.port']) ? (';port=' . $params['db.port']) : ''),
+        'username'  => $params['db.user'],
+        'password'  => $params['db.password'],
+    ],
 ];
--- config/params.php
+++ config/params.php
@@ -1,4 +1,9 @@
 <?php
 
 return [
+    'db.name'       => null,
+    'db.host'       => null,
+    'db.port'       => null,
+    'db.user'       => null,
+    'db.password'   => null,
 ];
