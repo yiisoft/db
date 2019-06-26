958906ebb85fc614314d060164e3b97d3b2e098f
Дмитрий Егоров <desure85@gmail.com>
2019-06-26 08:33:43 +0300

    \yii\di\Reference -> Yiisoft\Factory\Definitions\Reference

--- config/common.php
+++ config/common.php
@@ -1,12 +1,16 @@
 <?php
 
+use Yiisoft\Db\Connection;
+use Yiisoft\Db\ConnectionInterface;
+use Yiisoft\Factory\Definitions\Reference;
+
 return [
-    \Yiisoft\Db\ConnectionInterface::class => \yii\di\Reference::to('db'),
+    ConnectionInterface::class => Reference::to('db'),
     'db'                               => [
-        '__class'   => \Yiisoft\Db\Connection::class,
+        '__class'   => Connection::class,
         'dsn'       => 'pgsql:dbname='.$params['db.name']
-                        .(!empty($params['db.host']) ? (';host='.$params['db.host']) : '')
-                        .(!empty($params['db.port']) ? (';port='.$params['db.port']) : ''),
+            .(!empty($params['db.host']) ? (';host='.$params['db.host']) : '')
+            .(!empty($params['db.port']) ? (';port='.$params['db.port']) : ''),
         'username'  => $params['db.user'],
         'password'  => $params['db.password'],
     ],
