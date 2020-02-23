47e1d9279b563e5f0eb5b93d53d7a39dc19a0239
terabytesoftw <terabytesoftw@gmail.com>
2020-02-23 13:20:28 -0300

    Fix minor correction.

--- src/Helper/Dsn.php
+++ src/Helper/Dsn.php
@@ -13,7 +13,7 @@ final class Dsn
     private ?string $port;
     private array $options;
 
-    public function __construct(string $driver, string $host = null, string $dbname = null, string $port = null, array $options = [])
+    public function __construct(string $driver, string $host, string $dbname, string $port = null, array $options = [])
     {
         $this->driver = $driver;
         $this->host = $host;
@@ -24,11 +24,7 @@ final class Dsn
 
     public function getDsn(): string
     {
-        if ($this->driver !== 'sqlite') {
-            $this->dsn = "$this->driver:" . "host=$this->host" . ';' . "dbname=$this->dbname";
-        } else {
-            $this->dsn = "$this->driver:" . $this->dbname;
-        }
+        @$this->dsn = "$this->driver:" . "host=$this->host" . ';' . "dbname=$this->dbname";
 
         if ($this->port !== null) {
             $this->dsn = $this->dsn . ';' . "port=$this->port";
