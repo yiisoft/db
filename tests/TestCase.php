3b8968c468708a99e8fbebf8adcd0f6067efd2da
terabytesoftw <terabytesoftw@gmail.com>
2020-08-21 09:29:02 -0400

    Add NullLogger::class tests.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,6 +7,7 @@ namespace Yiisoft\Db\Tests;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 use Psr\Log\LogLevel;
+use Psr\Log\NullLogger;
 use Yiisoft\Aliases\Aliases;
 use Yiisoft\Cache\ArrayCache;
 use Yiisoft\Cache\Cache;
@@ -17,9 +18,6 @@ use Yiisoft\Db\Exception\InvalidConfigException;
 use Yiisoft\Di\Container;
 use Yiisoft\Files\FileHelper;
 use Yiisoft\Log\Logger;
-use Yiisoft\Log\Target\File\FileRotator;
-use Yiisoft\Log\Target\File\FileRotatorInterface;
-use Yiisoft\Log\Target\File\FileTarget;
 use Yiisoft\Profiler\Profiler;
 
 abstract class TestCase extends \PHPUnit\Framework\TestCase
@@ -178,33 +176,7 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
                 return new Cache(new ArrayCache());
             },
 
-            FileRotatorInterface::class => static function () {
-                return new FileRotator(10);
-            },
-
-            LoggerInterface::class => static function (ContainerInterface $container) {
-                $aliases = $container->get(Aliases::class);
-                $fileRotator = $container->get(FileRotatorInterface::class);
-
-                $fileTarget = new FileTarget(
-                    $aliases->get('@runtime/logs/app.log'),
-                    $fileRotator
-                );
-
-                $fileTarget->setLevels(
-                    [
-                        LogLevel::EMERGENCY,
-                        LogLevel::ERROR,
-                        LogLevel::WARNING,
-                        LogLevel::INFO,
-                        LogLevel::DEBUG
-                    ]
-                );
-
-                return new Logger([
-                    'file' => $fileTarget,
-                ]);
-            },
+            LoggerInterface::class => NullLogger::class,
 
             Profiler::class => static function (ContainerInterface $container) {
                 return new Profiler($container->get(LoggerInterface::class));
