3c119980c252b53ceffa65473e745ecb1b9ffd7e
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2020-10-09 18:48:50 -0300

    Fix psalm issues. (#182)
    * Fix psalm issues.
    
    * Fix minor correction.
    
    * Remove unnecessary phpdoc.

--- src/Schema/Schema.php
+++ src/Schema/Schema.php
@@ -100,6 +100,8 @@ abstract class Schema
         $this->cache = $this->db->getSchemaCache();
     }
 
+    abstract public function createQueryBuilder(): QueryBuilder;
+
     /**
      * Resolves the table name and schema name (if any).
      *
@@ -122,11 +124,11 @@ abstract class Schema
      * This method should be overridden by child classes in order to support this feature because the default
      * implementation simply throws an exception.
      *
-     * @return void all schema names in the database, except system schemas.
+     * @return array all schema names in the database, except system schemas.
      *
      * @throws NotSupportedException if this method is not supported by the DBMS.
      */
-    protected function findSchemaNames()
+    protected function findSchemaNames(): array
     {
         throw new NotSupportedException(get_class($this) . ' does not support fetching all schema names.');
     }
@@ -139,7 +141,7 @@ abstract class Schema
      *
      * @param string $schema the schema of the tables. Defaults to empty string, meaning the current or default schema.
      *
-     * @return void all table names in the database. The names have NO schema name prefix.
+     * @return array all table names in the database. The names have NO schema name prefix.
      *
      * @throws NotSupportedException if this method is not supported by the DBMS.
      */
