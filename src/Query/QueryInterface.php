26a0e187bec45909a5178496357db7fd5e9cfe6c
Wilmer <terabytesoftw@gmail.com>
2021-09-29 20:02:38 -0300

    Interface 'QueryInterface' Missing 'select' method declaration (#221).

--- src/Query/QueryInterface.php
+++ src/Query/QueryInterface.php
@@ -288,4 +288,28 @@ interface QueryInterface
      * @return QueryInterface the query object itself.
      */
     public function emulateExecution(bool $value = true): self;
+
+    /**
+     * Sets the SELECT part of the query.
+     *
+     * @param array|ExpressionInterface|string $columns the columns to be selected.
+     * Columns can be specified in either a string (e.g. "id, name") or an array (e.g. ['id', 'name']).
+     * Columns can be prefixed with table names (e.g. "user.id") and/or contain column aliases
+     * (e.g. "user.id AS user_id").
+     *
+     * The method will automatically quote the column names unless a column contains some parenthesis (which means the
+     * column contains a DB expression). A DB expression may also be passed in form of an {@see ExpressionInterface}
+     * object.
+     *
+     * Note that if you are selecting an expression like `CONCAT(first_name, ' ', last_name)`, you should use an array
+     * to specify the columns. Otherwise, the expression may be incorrectly split into several parts.
+     *
+     * When the columns are specified as an array, you may also use array keys as the column aliases (if a column does
+     * not need alias, do not use a string key).
+     * @param string|null $option additional option that should be appended to the 'SELECT' keyword. For example,
+     * in MySQL, the option 'SQL_CALC_FOUND_ROWS' can be used.
+     *
+     * @return $this the query object itself.
+     */
+    public function select($columns, ?string $option = null): self;
 }
