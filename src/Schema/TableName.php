e90cfb75d0208f3e5f9fdac3b4b95a512c4df290
Valerii Gorbachev <darkdef@gmail.com>
2021-10-26 07:03:33 +0300

    Update src/Schema/TableName.php
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

--- src/Schema/TableName.php
+++ src/Schema/TableName.php
@@ -41,7 +41,7 @@ final class TableName
     {
         if (!is_string($name) && !$name instanceof ExpressionInterface) {
             throw new InvalidArgumentException(
-                'Name of table should be string or instanceof ExpressionInterface'
+                'Name of the table should be a string or an instanceof ExpressionInterface.'
             );
         }
 
