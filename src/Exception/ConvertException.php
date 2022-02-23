848c97f3be95e3f1c6c4e4db712d2bb528e45c31
Wilmer <terabytesoftw@gmail.com>
2022-02-23 14:08:37 -0300

    Apply sugestion review.

--- src/Exception/ConvertException.php
+++ src/Exception/ConvertException.php
@@ -17,21 +17,15 @@ final class ConvertException
 
     public function run(): Exception
     {
-        if ($this->e instanceof Exception) {
-            return $this->e;
-        }
-
         $message = $this->e->getMessage() . PHP_EOL . 'The SQL being executed was: ' . $this->rawSql;
         $errorInfo = $this->e instanceof PDOException ? $this->e->errorInfo : null;
-        $exception = new Exception($message, $errorInfo, $this->e);
 
-        if (
-            str_contains($message, self::MSG_INTEGRITY_EXCEPTION_1)
-            || str_contains($message, self::MGS_INTEGRITY_EXCEPTION_2)
+        return match (
+            str_contains($message, self::MSG_INTEGRITY_EXCEPTION_1) ||
+            str_contains($message, self::MGS_INTEGRITY_EXCEPTION_2)
         ) {
-            $exception = new IntegrityException($message, $errorInfo, $this->e);
-        }
-
-        return $exception;
+            true => new IntegrityException($message, $errorInfo, $this->e),
+            default => new Exception($message, $errorInfo, $this->e),
+        };
     }
 }
