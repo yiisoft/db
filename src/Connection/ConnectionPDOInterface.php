b1904518c9e4a874e88ed55458e690452a358fe2
darkdef <darkdef@gmail.com>
2022-04-16 12:18:32 +0300

    Removing getDriver from ConnectionPDO classes

--- src/Connection/ConnectionPDOInterface.php
+++ src/Connection/ConnectionPDOInterface.php
@@ -12,11 +12,6 @@ use Yiisoft\Db\Exception\InvalidConfigException;
 interface ConnectionPDOInterface extends ConnectionInterface
 {
     /**
-     * Returns the currently active driver connection.
-     */
-    public function getDriver(): PDODriver;
-
-    /**
      * The PHP PDO instance associated with this DB connection. This property is mainly managed by {@see open()} and
      * {@see close()} methods. When a DB connection is active, this property will represent a PDO instance; otherwise,
      * it will be null.
