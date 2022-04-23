d630f11c628f186180d9d1411d42a87b64f954e4
darkdef <darkdef@gmail.com>
2022-04-23 19:54:56 +0300

    psalm fix

--- src/Schema/Schema.php
+++ src/Schema/Schema.php
@@ -511,7 +511,7 @@ abstract class Schema implements SchemaInterface
      * @param string $name
      * @param bool $refresh
      *
-     * @return Constraint|Constraint[]|TableSchema|null
+     * @return array|Constraint|TableSchema|null
      */
     protected function getTableTypeMetadata(
         string $type,
