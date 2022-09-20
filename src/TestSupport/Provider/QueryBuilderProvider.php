a5450ec5a8c42ebb29eb29ab8a762f9acd7a24af
Valerii Gorbachev <darkdef@gmail.com>
2022-09-20 19:49:16 +0300

    Refactoring of batchInsert (for remove using of Quoter) (#363)
    * Refactoring of batchInsert (for remove using of Quoter)
    
    * styleci fixes
    
    * styleci fixes

--- src/TestSupport/Provider/QueryBuilderProvider.php
+++ src/TestSupport/Provider/QueryBuilderProvider.php
@@ -129,60 +129,82 @@ final class QueryBuilderProvider
     public function batchInsertProvider(): array
     {
         return [
-            [
+            'simple' => [
                 'customer',
                 ['email', 'name', 'address'],
                 [['test@example.com', 'silverfire', 'Kyiv {{city}}, Ukraine']],
-                DbHelper::replaceQuotes(
+                'expected' => DbHelper::replaceQuotes(
                     'INSERT INTO [[customer]] ([[email]], [[name]], [[address]])'
-                    . " VALUES ('test@example.com', 'silverfire', 'Kyiv {{city}}, Ukraine')",
+                    . ' VALUES (:qp0, :qp1, :qp2)',
                     $this->db->getDriver()->getDriverName(),
                 ),
+                [
+                    ':qp0' => 'test@example.com',
+                    ':qp1' => 'silverfire',
+                    ':qp2' => 'Kyiv {{city}}, Ukraine',
+                ],
             ],
             'escape-danger-chars' => [
                 'customer',
                 ['address'],
                 [["SQL-danger chars are escaped: '); --"]],
                 'expected' => DbHelper::replaceQuotes(
-                    "INSERT INTO [[customer]] ([[address]]) VALUES ('SQL-danger chars are escaped: \'); --')",
+                    'INSERT INTO [[customer]] ([[address]]) VALUES (:qp0)',
                     $this->db->getDriver()->getDriverName(),
                 ),
+                [
+                    ':qp0' => "SQL-danger chars are escaped: '); --",
+                ],
             ],
-            [
+            'customer2' => [
                 'customer',
                 ['address'],
                 [],
                 '',
             ],
-            [
+            'customer3' => [
                 'customer',
                 [],
                 [['no columns passed']],
-                DbHelper::replaceQuotes(
-                    "INSERT INTO [[customer]] () VALUES ('no columns passed')",
+                'expected' => DbHelper::replaceQuotes(
+                    'INSERT INTO [[customer]] () VALUES (:qp0)',
                     $this->db->getDriver()->getDriverName(),
                 ),
+                [
+                    ':qp0' => 'no columns passed',
+                ],
             ],
             'bool-false, bool2-null' => [
                 'type',
                 ['bool_col', 'bool_col2'],
                 [[false, null]],
                 'expected' => DbHelper::replaceQuotes(
-                    'INSERT INTO [[type]] ([[bool_col]], [[bool_col2]]) VALUES (0, NULL)',
+                    'INSERT INTO [[type]] ([[bool_col]], [[bool_col2]]) VALUES (:qp0, :qp1)',
                     $this->db->getDriver()->getDriverName(),
                 ),
+                [
+                    ':qp0' => 0,
+                    ':qp1' => null,
+                ],
             ],
-            [
+            'wrong' => [
                 '{{%type}}',
                 ['{{%type}}.[[float_col]]', '[[time]]'],
-                [[null, new Expression('now()')]],
-                'INSERT INTO {{%type}} ({{%type}}.[[float_col]], [[time]]) VALUES (NULL, now())',
+                [[null, new Expression('now()')], [null, new Expression('now()')]],
+                'expected' => 'INSERT INTO {{%type}} ({{%type}}.[[float_col]], [[time]]) VALUES (:qp0, now()), (:qp1, now())',
+                [
+                    ':qp0' => null,
+                    ':qp1' => null,
+                ],
             ],
             'bool-false, time-now()' => [
                 '{{%type}}',
                 ['{{%type}}.[[bool_col]]', '[[time]]'],
                 [[false, new Expression('now()')]],
-                'expected' => 'INSERT INTO {{%type}} ({{%type}}.[[bool_col]], [[time]]) VALUES (0, now())',
+                'expected' => 'INSERT INTO {{%type}} ({{%type}}.[[bool_col]], [[time]]) VALUES (:qp0, now())',
+                [
+                    ':qp0' => null,
+                ],
             ],
         ];
     }
