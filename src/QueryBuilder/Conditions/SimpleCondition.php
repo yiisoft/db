85884f3b6d17ea92fd5ac36d1e8d31316a2dac3a
Sartor <sartor@syneforge.com>
2022-11-01 09:45:39 +0200

    Mixed type for value in SimpleCondition

--- src/QueryBuilder/Conditions/SimpleCondition.php
+++ src/QueryBuilder/Conditions/SimpleCondition.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\QueryBuilder\Conditions;
 
-use Iterator;
 use Yiisoft\Db\Exception\InvalidArgumentException;
 use Yiisoft\Db\Expression\Expression;
-use Yiisoft\Db\Expression\ExpressionInterface;
 use Yiisoft\Db\QueryBuilder\Conditions\Interface\SimpleConditionInterface;
 use Yiisoft\Db\Query\QueryInterface;
 
@@ -21,7 +19,7 @@ final class SimpleCondition implements SimpleConditionInterface
     public function __construct(
         private string|Expression|QueryInterface $column,
         private string $operator,
-        private array|int|bool|string|Iterator|ExpressionInterface|null $value
+        private mixed $value
     ) {
     }
 
@@ -35,7 +33,7 @@ final class SimpleCondition implements SimpleConditionInterface
         return $this->operator;
     }
 
-    public function getValue(): array|int|bool|string|Iterator|ExpressionInterface|null
+    public function getValue(): mixed
     {
         return $this->value;
     }
