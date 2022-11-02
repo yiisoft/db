f1d591baa81b861570801a154d71f2be1e9f5d7a
Wilmer Arambula <terabytesoftw@gmail.com>
2022-11-02 12:23:47 -0300

    Raise code coverage 100% `ConjunctionConditionBuilder::class`.

--- tests/Provider/QueryBuilderProvider.php
+++ tests/Provider/QueryBuilderProvider.php
@@ -34,6 +34,8 @@ abstract class QueryBuilderProvider
             ],
 
             /* and */
+            [['and', '', ''], '', []],
+            [['and', '', 'id=2'], 'id=2', []],
             [['and', 'id=1', 'id=2'], '(id=1) AND (id=2)', []],
             [['and', 'type=1', ['or', 'id=1', 'id=2']], '(type=1) AND ((id=1) OR (id=2))', []],
             [['and', 'id=1', new Expression('id=:qp0', [':qp0' => 2])], '(id=1) AND (id=:qp0)', [':qp0' => 2]],
