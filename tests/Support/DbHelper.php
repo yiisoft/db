d339f1e3971f2a915313ed9cdf84e0aeb3c3636c
Wilmer Arambula <terabytesoftw@gmail.com>
2022-11-23 08:04:22 -0300

    Move methods to `DbHelper::class`.

--- tests/Support/DbHelper.php
+++ tests/Support/DbHelper.php
@@ -4,8 +4,56 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Tests\Support;
 
+use Yiisoft\Cache\ArrayCache;
+use Yiisoft\Cache\Cache;
+use Yiisoft\Cache\CacheInterface;
+use Yiisoft\Db\Cache\QueryCache;
+use Yiisoft\Db\Cache\SchemaCache;
+use Yiisoft\Db\Driver\PDO\ConnectionPDOInterface;
+use Yiisoft\Db\Exception\Exception;
+use Yiisoft\Db\Exception\InvalidConfigException;
+
+use function explode;
+use function file_get_contents;
+use function preg_replace;
+use function str_replace;
+use function trim;
+
 final class DbHelper
 {
+    public static function getCache(): CacheInterface
+    {
+        return new Cache(new ArrayCache());
+    }
+
+    public static function getQueryCache(): QueryCache
+    {
+        return new QueryCache(self::getCache());
+    }
+
+    public static function getSchemaCache(): SchemaCache
+    {
+        return new SchemaCache(self::getCache());
+    }
+
+    /**
+     * Loads the fixture into the database.
+     *
+     * @throws Exception
+     * @throws InvalidConfigException
+     */
+    public static function loadFixture(ConnectionPDOInterface $db, string $fixture): void
+    {
+        $db->open();
+        $lines = explode(';', file_get_contents($fixture));
+
+        foreach ($lines as $line) {
+            if (trim($line) !== '') {
+                $db->getPDO()?->exec($line);
+            }
+        }
+    }
+
     /**
      * Adjust dbms specific escaping.
      *
--- tests/Support/TestTrait.php
+++ tests/Support/TestTrait.php
@@ -4,14 +4,8 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Tests\Support;
 
-use Yiisoft\Cache\ArrayCache;
-use Yiisoft\Cache\Cache;
-use Yiisoft\Cache\CacheInterface;
-use Yiisoft\Db\Cache\QueryCache;
-use Yiisoft\Db\Cache\SchemaCache;
 use Yiisoft\Db\Driver\PDO\ConnectionPDOInterface;
-use Yiisoft\Db\Exception\Exception;
-use Yiisoft\Db\Exception\InvalidConfigException;
+use Yiisoft\Db\Tests\Support\DbHelper;
 use Yiisoft\Db\Tests\Support\Stub\PDODriver;
 
 trait TestTrait
@@ -22,55 +16,12 @@ trait TestTrait
 
     protected function getConnection(string $fixture = '', string $dsn = 'sqlite::memory:'): ConnectionPDOInterface
     {
-        $db = new Stub\Connection(new PDODriver($dsn), $this->getQueryCache(), $this->getSchemaCache());
+        $db = new Stub\Connection(new PDODriver($dsn), DbHelper::getQueryCache(), DbHelper::getSchemaCache());
 
         if ($fixture !== '') {
-            $this->loadFixture($db, __DIR__ . "/Fixture/$fixture.sql");
+            DbHelper::loadFixture($db, __DIR__ . "/Fixture/$fixture.sql");
         }
 
         return $db;
-    }
-
-    private function getCache(): CacheInterface
-    {
-        if ($this->cache === null) {
-            $this->cache = new Cache(new ArrayCache());
-        }
-
-        return $this->cache;
-    }
-
-    private function getQueryCache(): QueryCache
-    {
-        if ($this->queryCache === null) {
-            $this->queryCache = new QueryCache($this->getCache());
-        }
-
-        return $this->queryCache;
-    }
-
-    private function getSchemaCache(): SchemaCache
-    {
-        if ($this->schemaCache === null) {
-            $this->schemaCache = new SchemaCache($this->getCache());
-        }
-
-        return $this->schemaCache;
-    }
-
-    /**
-     * @throws Exception
-     * @throws InvalidConfigException
-     */
-    public function loadFixture(ConnectionPDOInterface $db, string $fixture): void
-    {
-        $db->open();
-        $lines = explode(';', file_get_contents($fixture));
-
-        foreach ($lines as $line) {
-            if (trim($line) !== '') {
-                $db->getPDO()?->exec($line);
-            }
-        }
     }
 }
