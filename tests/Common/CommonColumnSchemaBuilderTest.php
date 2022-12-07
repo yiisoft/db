df027fd37604b1168839f2c7c5afe556fe7f6ce1
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2022-12-07 17:56:09 -0300

    Remove src\TestSupport\TestColumnSchemaBuilderTrait from yiisoft\db. (#408)
    * Remove src\TestSupport\TestColumnSchemaBuilderTrait from yiisoft\db.

---
+++ tests/Common/CommonColumnSchemaBuilderTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Db\Tests\Common;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Db\Tests\Support\TestTrait;
+
+use function array_shift;
+use function call_user_func_array;
+
+abstract class CommonColumnSchemaBuilderTest extends TestCase
+{
+    use TestTrait;
+
+    /**
+     * @dataProvider \Yiisoft\Db\Tests\Provider\ColumnSchemaBuilderProvider::types();
+     */
+    public function testCustomTypes(string $expected, string $type, int|null $length, array $calls): void
+    {
+        $this->checkBuildString($expected, $type, $length, $calls);
+    }
+
+    protected function checkBuildString(string $expected, string $type, int|null $length, array $calls): void
+    {
+        $db = $this->getConnection();
+
+        $schema = $db->getSchema();
+        $builder = $schema->createColumnSchemaBuilder($type, $length);
+
+        foreach ($calls as $call) {
+            $method = array_shift($call);
+            call_user_func_array([$builder, $method], $call);
+        }
+
+        $this->assertSame($expected, $builder->__toString());
+    }
+}
