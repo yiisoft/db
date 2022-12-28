44ed4cc1b7e1656e985a1a3cfee17feb543be2d0
Wilmer Arambula <terabytesoftw@gmail.com>
2022-12-28 15:42:11 -0300

    Add more tests.

--- tests/AbstractQueryTest.php
+++ tests/AbstractQueryTest.php
@@ -52,6 +52,32 @@ abstract class AbstractQueryTest extends TestCase
     }
 
     /**
+     * Ensure no ambiguous column error occurs on indexBy with JOIN.
+     *
+     * @link https://github.com/yiisoft/yii2/issues/13859
+     */
+    public function testAmbiguousColumnIndexBy()
+    {
+        $db = $this->getConnection(true);
+
+        $selectExpression = match ($db->getName()) {
+            'mysql' => "concat(customer.name,' in ', p.description) name",
+            'oci' => "[[customer]].[[name]] || ' in ' || [[p]].[[description]] name",
+            'pgsql', 'sqlite' => "(customer.name || ' in ' || p.description) AS name",
+            'sqlsrv'=> 'CONCAT(customer.name, \' in \', p.description) name',
+        };
+
+        $result = (new Query($db))
+            ->select([$selectExpression])
+            ->from('customer')
+            ->innerJoin('profile p', '[[customer]].[[profile_id]] = [[p]].[[id]]')
+            ->indexBy('id')
+            ->column();
+
+        $this->assertSame([1 => 'user1 in profile customer 1', 3 => 'user3 in profile customer 3'], $result);
+    }
+
+    /**
      * @depends testFilterWhereWithHashFormat
      * @depends testFilterWhereWithOperatorFormat
      *
