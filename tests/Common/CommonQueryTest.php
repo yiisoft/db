4c4b45a1853ee9861f5c9d9ee9f070ecce5085dd
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-01-02 09:54:13 -0300

    Fix phpdoc, add more tests. (#440)
    * Fix phpdoc Query::class, QueryInterface::class, QueryPartsInterface::class.
    * Raise code coverage 100% Query::class.

---
+++ tests/Common/CommonQueryTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Db\Tests\Common;
+
+use Yiisoft\Db\Query\Query;
+use Yiisoft\Db\Tests\AbstractQueryTest;
+
+abstract class CommonQueryTest extends AbstractQueryTest
+{
+    public function testColumnWithIndexBy(): void
+    {
+        $db = $this->getConnection(true);
+
+        $query = (new Query($db))->select('customer.name')->from('customer')->indexBy('customer.id');
+
+        $this->assertSame([1 => 'user1', 2 => 'user2', 3 => 'user3'], $query->column());
+    }
+
+    public function testColumnIndexByWithClosure()
+    {
+        $db = $this->getConnection(true);
+
+        $result = (new Query($db))
+            ->select(['id', 'name'])
+            ->from('customer')
+            ->indexBy(fn ($row) => $row['id'] * 2)
+            ->column();
+
+        $this->assertEquals([2 => '1', 4 => '2', 6 => '3'], $result);
+    }
+}
