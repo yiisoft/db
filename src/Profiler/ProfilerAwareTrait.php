1eff82723aba032342bc9d279f7836387a726288
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-01-13 18:31:55 -0300

    Update src/Profiler/ProfilerAwareTrait.php
    Update src/Profiler/ProfilerAwareTrait.php
    
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

--- src/Profiler/ProfilerAwareTrait.php
+++ src/Profiler/ProfilerAwareTrait.php
@@ -5,9 +5,7 @@ declare(strict_types=1);
 namespace Yiisoft\Db\Profiler;
 
 /**
- * The ProfilerAwareTrait::class is a trait, allows a class to be aware of the database profiler component. When this
- * trait is used, the class will have access to the database profiler, which can be used to collect and analyze data
- * about database queries. This can be useful for debugging and optimizing database performance.
+ * When this trait is used, the class will have access to the database profiler, a tool for collecting and analyzing database queries. This can be useful for debugging and optimizing database performance.
  */
 trait ProfilerAwareTrait
 {
