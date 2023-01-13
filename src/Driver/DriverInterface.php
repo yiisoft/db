75622027382f65e8c6a804ba7c6ea22b0a12c89b
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-01-13 18:14:46 -0300

    Update src/Driver/DriverInterface.php
    Update src/Driver/DriverInterface.php
    
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

--- src/Driver/DriverInterface.php
+++ src/Driver/DriverInterface.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Yiisoft\Db\Driver;
 
 /**
- * The DriverInterface class is an interface which represents a database driver. A driver is a specific implementation
+ * The DriverInterface represents a database driver. A specific implementation
  * of a database connectivity layer, such as `PDO` or `MySQLi`. The interface defines the methods that must be
  * implemented by a concrete driver class, such as methods for connecting to a database, executing queries, and managing
  * transactions.
