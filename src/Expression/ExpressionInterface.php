af9e3424347bf4b7321ceb95caf340e1fd8174b5
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-01-13 18:30:51 -0300

    Update src/Expression/ExpressionInterface.php
    Update src/Expression/ExpressionInterface.php
    
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

--- src/Expression/ExpressionInterface.php
+++ src/Expression/ExpressionInterface.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Yiisoft\Db\Expression;
 
 /**
- * The ExpressionInterface class defines a set of methods that an object should implement in order to be used as an
- * expression in database queries, such as for filtering or ordering results. These methods include methods for
+ * The ExpressionInterface defines a set of methods that an object should implement in order to be used as an
+ * expression in database queries, such as for filtering or ordering results. These methods include
  * getting the expression as a string, getting the parameters to bind to the expression, and getting the types of the
  * parameters. Classes that implement this interface can be used in a variety of query building methods provided by
  * the library.
