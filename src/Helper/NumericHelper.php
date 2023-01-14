b34710856fd3abd30d8c3a024db8f6d09a7f8ef6
darkdef <darkdef@gmail.com>
2023-01-14 11:23:39 +0300

    remove unused code

--- src/Helper/NumericHelper.php
+++ src/Helper/NumericHelper.php
@@ -11,25 +11,16 @@ final class NumericHelper
     /**
      * Returns string representation of a number value without thousands separators and with dot as decimal separator.
      *
-     * @param bool|float|int|string $value
-     *
-     * @throws InvalidArgumentException if value is not scalar.
+     * @param float|string $value
      *
      * @return string
      */
-    public static function normalize($value): string
+    public static function normalize(float|string $value): string
     {
-        /** @psalm-suppress DocblockTypeContradiction */
-        if (!is_scalar($value)) {
-            $type = gettype($value);
-            throw new InvalidArgumentException("Value must be scalar. $type given.");
-        }
-
-        if (is_bool($value)) {
-            $value = $value ? '1' : '0';
-        } else {
+        if (is_float($value)) {
             $value = (string)$value;
         }
+
         $value = str_replace([' ', ','], ['', '.'], $value);
         return preg_replace('/\.(?=.*\.)/', '', $value);
     }
