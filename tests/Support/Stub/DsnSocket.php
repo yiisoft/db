9cf2452e9b47038b792bba056f7ab4dfa9d4e3a4
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-02-18 14:48:39 -0300

    Add `AbstractDsnSocket::class`. (#533)

---
+++ tests/Support/Stub/DsnSocket.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Db\Tests\Support\Stub;
+
+use Yiisoft\Db\Connection\AbstractDsnSocket;
+
+final class DsnSocket extends AbstractDsnSocket
+{
+}
