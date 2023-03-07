2384697a265414600c72af60c085c03af84b8bd4
Wilmer Arambula <terabytesoftw@gmail.com>
2023-03-07 10:08:23 -0300

    Fix phpdoc `QueryBuilder` classes.

--- src/QueryBuilder/QueryBuilderInterface.php
+++ src/QueryBuilder/QueryBuilderInterface.php
@@ -10,10 +10,20 @@ use Yiisoft\Db\Expression\ExpressionInterface;
 use Yiisoft\Db\Schema\ColumnSchemaBuilderInterface;
 use Yiisoft\Db\Schema\QuoterInterface;
 
+/**
+ * Defines the common interface to be implemented by query builder classes.
+ *
+ * A query builder is mainly responsible for creating SQL statements based on specifications given in a
+ * {@see QueryInterface} object.
+ *
+ * A query builder should also support creating SQL statements for DBMS-specific features such as creating indexes,
+ * adding foreign keys, etc. through the methods defined in {@see DDLQueryBuilderInterface}, {@see DMLQueryBuilderInterface}
+ * and {@see DQLQueryBuilderInterface}.
+ */
 interface QueryBuilderInterface extends DDLQueryBuilderInterface, DMLQueryBuilderInterface, DQLQueryBuilderInterface
 {
     /**
-     * Helper method to add $value to $params array using {@see PARAM_PREFIX}.
+     * Helper method to add `$value` to `$params` array using {@see PARAM_PREFIX}.
      *
      * @param array $params Passed by reference.
      *
@@ -30,9 +40,9 @@ interface QueryBuilderInterface extends DDLQueryBuilderInterface, DMLQueryBuilde
      *
      * - `pk`: an auto-incremental primary key type, will be converted into "int(11) NOT NULL AUTO_INCREMENT PRIMARY
      *    KEY"
-     * - `bigpk`: an auto-incremental primary key type, will be converted into "bigint(20) NOT NULL AUTO_INCREMENT
+     * - `bigpk`: an auto incremental primary key type, will be converted into "bigint(20) NOT NULL AUTO_INCREMENT
      *    PRIMARY KEY"
-     * - `upk`: an unsigned auto-incremental primary key type, will be converted into "int(10) UNSIGNED NOT NULL
+     * - `upk`: an unsigned auto incremental primary key type, will be converted into "int(10) UNSIGNED NOT NULL
      *    AUTO_INCREMENT PRIMARY KEY"
      * - `char`: char type, will be converted into "char(1)"
      * - `string`: string type, will be converted into "varchar(255)"
@@ -50,18 +60,18 @@ interface QueryBuilderInterface extends DDLQueryBuilderInterface, DMLQueryBuilde
      * - `money`: money type, will be converted into "decimal(19,4)"
      * - `binary`: binary data type, will be converted into "blob"
      *
-     * If the abstract type contains two or more parts separated by spaces (e.g. "string NOT NULL"), then only the first
+     * If the abstract type has two or more parts separated by spaces (e.g. "string NOT NULL"), then only the first
      * part will be converted, and the rest of the parts will be appended to the converted result.
      *
      * For example, 'string NOT NULL' is converted to 'varchar(255) NOT NULL'.
      *
-     * For some of the abstract types you can also specify a length or precision constraint by appending it in round
-     * brackets directly to the type.
+     * For some abstract types, you can also specify a length or precision constraint by appending it in round brackets
+     * directly to the type.
      *
-     * For example `string(32)` will be converted into "varchar(32)" on a MySQL database. If the underlying DBMS does
-     * not support these kind of constraints for a type it will be ignored.
+     * For example, `string(32)` will be converted into "varchar(32)" on a MySQL database.
+     * If the underlying DBMS doesn't support these kinds of constraints for a type, it will be ignored.
      *
-     * If a type cannot be found in {@see typeMap}, it will be returned without any change.
+     * If a type can't be found in {@see typeMap}, it will be returned without any change.
      *
      * @param ColumnSchemaBuilderInterface|string $type Abstract column type.
      *
@@ -70,20 +80,18 @@ interface QueryBuilderInterface extends DDLQueryBuilderInterface, DMLQueryBuilde
     public function getColumnType(ColumnSchemaBuilderInterface|string $type): string;
 
     /**
-     * Gets object of {@see ExpressionBuilderInterface} that is suitable for $expression.
+     * Gets an object of {@see ExpressionBuilderInterface} that's suitable for $expression.
      *
-     * Uses {@see expressionBuilders} array to find a suitable builder class.
+     * Uses {@see AbstractDQLQueryBuilder::expressionBuilders} an array to find a suitable builder class.
      *
-     * @param ExpressionInterface $expression Expression to build.
+     * @param ExpressionInterface $expression The expression to build.
      *
-     * @throws InvalidArgumentException When expression building is not supported by this QueryBuilder.
-     *
-     * @see expressionBuilders
+     * @throws InvalidArgumentException When expression building isn't supported by this QueryBuilder.
      */
     public function getExpressionBuilder(ExpressionInterface $expression): object;
 
     /**
-     * @return QuoterInterface Quoter instance.
+     * @return QuoterInterface The quoter instance.
      */
     public function quoter(): QuoterInterface;
 }
