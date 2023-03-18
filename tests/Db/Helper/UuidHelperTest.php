740b9bba83e5e8568bf5cde11f03446ec24b6133
darkdef <darkdef@gmail.com>
2023-03-18 10:33:26 +0300

    add tests for UuidHelper

---
+++ tests/Db/Helper/UuidHelperTest.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Db\Tests\Db\Helper;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Db\Exception\InvalidArgumentException;
+use Yiisoft\Db\Helper\UuidHelper;
+
+/**
+ * @group db
+ */
+final class UuidHelperTest extends TestCase
+{
+    /**
+     * @dataProvider successUuids
+     */
+    public function testConvert(string $uuid): void
+    {
+        $blobUuid = UuidHelper::uuidToBlob($uuid);
+        $this->assertEquals($uuid, UuidHelper::toUuid($blobUuid));
+    }
+
+    /**
+     * @dataProvider incorrectUuids
+     */
+    public function testToBlobIncorrectUuid(string $uuid): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Incorrect UUID.');
+
+        $blobUuid = UuidHelper::uuidToBlob($uuid);
+        $this->assertEquals($uuid, UuidHelper::toUuid($blobUuid));
+    }
+
+    /**
+     * @dataProvider blobUuids
+     */
+    public function testToUuid($blobUuid, $expected): void
+    {
+        $uuid = UuidHelper::toUuid($blobUuid);
+        $this->assertEquals($expected, $uuid);
+    }
+
+    /**
+     * @dataProvider incorrectBlobUuids
+     */
+    public function testToUuidFailed($blobUuid, $expected): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Length of source data is should be 16 or 32 bytes.');
+
+        $uuid = UuidHelper::toUuid($blobUuid);
+        $this->assertEquals($expected, $uuid);
+    }
+
+    public function successUuids(): array
+    {
+        return [
+            ['738146be-87b1-49f2-9913-36142fb6fcbe']
+        ];
+    }
+
+    public function incorrectUuids(): array
+    {
+        return [
+            ['738146be-87b149f2-9913-36142fb6fcbe'],
+            ['738146be-87b1-K9f2-9913-36142fb6fcbe'],
+            ['738146be+87b1-K9f2-9913-36142fb6fcbe'],
+        ];
+    }
+
+    public function blobUuids(): array
+    {
+        return [
+            ['738146be-87b1-49f2-9913-36142fb6fcbe', '738146be-87b1-49f2-9913-36142fb6fcbe'],
+            ['738146be87b149f2991336142fb6fcbe', '738146be-87b1-49f2-9913-36142fb6fcbe'],
+            [hex2bin('738146be87b149f2991336142fb6fcbe'), '738146be-87b1-49f2-9913-36142fb6fcbe'],
+        ];
+    }
+
+    public function incorrectBlobUuids(): array
+    {
+        return [
+            ['738146be-87b1-49f2-9913-36142fbfcbe', '738146be-87b1-49f2-9913-36142fb6fcbe'],
+            ['738146be87b149f2991336142fb6fcb', '738146be-87b1-49f2-9913-36142fb6fcbe'],
+            [hex2bin('738146be87b149f291336142fb6fcb'), '738146be-87b1-49f2-9913-36142fb6fcbe'],
+        ];
+    }
+}
