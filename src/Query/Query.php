9320d8ebce315b4ff7aee66e42702a3c04ab637b
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-04-04 05:48:12 -0400

    Fix #642: Remove unsed `Stringable` in `Query::class` (#644)

--- src/Query/Query.php
+++ src/Query/Query.php
@@ -29,7 +29,6 @@ use function key;
 use function preg_match;
 use function preg_split;
 use function reset;
-use function serialize;
 use function str_contains;
 use function strcasecmp;
 use function strlen;
@@ -91,14 +90,6 @@ class Query implements QueryInterface
 
     public function __construct(protected ConnectionInterface $db)
     {
-    }
-
-    /**
-     * Returns the SQL representation of Query.
-     */
-    public function __toString(): string
-    {
-        return serialize($this);
     }
 
     public function addGroupBy(array|string|ExpressionInterface $columns): static
