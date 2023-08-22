a6a7b64afc6589ea4394afb75b7511d6b1271a97
Tigrov <rrr-r@ya.ru>
2023-08-22 08:59:11 +0700

    Fix test issues

--- tests/Support/Stub/DMLQueryBuilder.php
+++ tests/Support/Stub/DMLQueryBuilder.php
@@ -4,8 +4,28 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Tests\Support\Stub;
 
+use Yiisoft\Db\Exception\NotSupportedException;
+use Yiisoft\Db\Query\QueryInterface;
 use Yiisoft\Db\QueryBuilder\AbstractDMLQueryBuilder;
 
 final class DMLQueryBuilder extends AbstractDMLQueryBuilder
 {
+    public function insertWithReturningPks(string $table, QueryInterface|array $columns, array &$params = []): string
+    {
+        throw new NotSupportedException(__METHOD__ . '() is not supported by this DBMS.');
+    }
+
+    public function resetSequence(string $table, int|string|null $value = null): string
+    {
+        throw new NotSupportedException(__METHOD__ . '() is not supported by this DBMS.');
+    }
+
+    public function upsert(
+        string $table,
+        QueryInterface|array $insertColumns,
+        bool|array $updateColumns,
+        array &$params
+    ): string {
+        throw new NotSupportedException(__METHOD__ . '() is not supported by this DBMS.');
+    }
 }
