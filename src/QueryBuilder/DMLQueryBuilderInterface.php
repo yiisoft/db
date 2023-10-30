8aa612af19830dfee7f4d79fc69101385904cc49
Tigrov <rrr-r@ya.ru>
2023-10-30 20:38:14 +0700

    Apply StyleCI

--- src/QueryBuilder/DMLQueryBuilderInterface.php
+++ src/QueryBuilder/DMLQueryBuilderInterface.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\QueryBuilder;
 
-use Generator;
 use JsonException;
 use Yiisoft\Db\Exception\Exception;
 use Yiisoft\Db\Exception\InvalidArgumentException;
