5136e62de1874e7698057120df08961df9c8bf85
Tigrov <rrr-r@ya.ru>
2023-11-23 09:49:31 +0700

    Remove redundant checks

--- src/Driver/Pdo/AbstractPdoCommand.php
+++ src/Driver/Pdo/AbstractPdoCommand.php
@@ -265,19 +265,18 @@ abstract class AbstractPdoCommand extends AbstractCommand implements PdoCommandI
 
     protected function queryInternal(int $queryMode): mixed
     {
-        $rawSql = '';
-
-        if ($this->logger !== null || $this->profiler !== null) {
-            $rawSql = $this->getRawSql();
-        }
-
         $logCategory = self::class . '::' . $this->getQueryMode($queryMode);
 
-        $this->logQuery($rawSql, $logCategory);
+        $this->logger?->log(LogLevel::INFO, $rawSql = $this->getRawSql(), [$logCategory]);
 
         $queryContext = new CommandContext(__METHOD__, $logCategory, $this->getSql(), $this->getParams());
 
-        $this->profiler?->begin($rawSql, $queryContext);
+        /**
+         * @psalm-var string $rawSql
+         * @psalm-suppress RedundantConditionGivenDocblockType
+         * @psalm-suppress DocblockTypeContradiction
+         */
+        $this->profiler?->begin($rawSql ??= $this->getRawSql(), $queryContext);
         try {
             /** @psalm-var mixed $result */
             $result = parent::queryInternal($queryMode);
