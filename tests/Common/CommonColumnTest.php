28c81ee9508ed4913021c90408315652a7c98ebc
Tigrov <rrr-r@ya.ru>
2025-05-04 16:18:11 +0700

    Fix test

--- tests/Common/CommonColumnTest.php
+++ tests/Common/CommonColumnTest.php
@@ -19,6 +19,7 @@ use Yiisoft\Db\Tests\Support\TestTrait;
 use function array_fill_keys;
 use function array_keys;
 use function array_map;
+use function version_compare;
 
 abstract class CommonColumnTest extends AbstractColumnTest
 {
@@ -62,7 +63,13 @@ abstract class CommonColumnTest extends AbstractColumnTest
         $utcTimezone = new DateTimeZone('UTC');
 
         $this->assertEquals(
-            new Expression($db->getDriverName() === 'sqlsrv' ? 'getdate()' : 'CURRENT_TIMESTAMP'),
+            new Expression(match($db->getDriverName()) {
+                'sqlsrv' => 'getdate()',
+                'pgsql' => version_compare($db->getServerInfo()->getVersion(), '9', '=')
+                    ? 'now()'
+                    : 'CURRENT_TIMESTAMP',
+                default => 'CURRENT_TIMESTAMP',
+            }),
             $columns['timestamp']->getDefaultValue(),
         );
         $this->assertEquals(new DateTimeImmutable('2025-04-19 14:11:35', $utcTimezone), $columns['datetime']->getDefaultValue());
