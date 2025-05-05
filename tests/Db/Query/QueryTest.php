3cfdc8dc849d9e534dee36376df191534b3c3b79
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-05-05 10:54:19 +0300

    Add `QueryTest`

--- tests/Db/Query/QueryTest.php
+++ tests/Db/Query/QueryTest.php
@@ -4,15 +4,16 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Tests\Db\Query;
 
-use Throwable;
-use Yiisoft\Db\Exception\Exception;
-use Yiisoft\Db\Exception\InvalidConfigException;
+use LogicException;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Yiisoft\Db\Exception\NotSupportedException;
 use Yiisoft\Db\Expression\Expression;
 use Yiisoft\Db\Query\Query;
 use Yiisoft\Db\Tests\AbstractQueryTest;
 use Yiisoft\Db\Tests\Support\Assert;
 use Yiisoft\Db\Tests\Support\TestTrait;
+
+use function PHPUnit\Framework\assertSame;
 
 /**
  * @group db
@@ -123,5 +124,111 @@ final class QueryTest extends AbstractQueryTest
         $command = $query->withTypecasting()->createCommand();
 
         $this->assertTrue(Assert::getInaccessibleProperty($command, 'phpTypecasting'));
+    }
+
+    public static function dataFor(): iterable
+    {
+        yield 'null' => [[], null];
+        yield 'empty-list' => [[], []];
+        yield 'empty-string' => [[''], ''];
+        yield 'string' => [['UPDATE'], 'UPDATE'];
+        yield 'list' => [['UPDATE', 'SHARE'], ['UPDATE', 'SHARE']];
+    }
+
+    #[DataProvider('dataFor')]
+    public function testFor(array $expected, array|string|null $value): void
+    {
+        $db = $this->getConnection();
+
+        $query = (new Query($db))->for($value);
+
+        assertSame($expected, $query->getFor());
+    }
+
+    public function testForTwice(): void
+    {
+        $db = $this->getConnection();
+
+        $query = (new Query($db))->for('UPDATE');
+
+        $this->expectException(LogicException::class);
+        $this->expectExceptionMessage('The `FOR` part was set earlier. Use the `setFor()` or `addFor()` method.');
+        $query->for('SHARE');
+    }
+
+    public static function dataAddFor(): iterable
+    {
+        yield 'null' => [['NO KEY UPDATE'], null];
+        yield 'empty-list' => [['NO KEY UPDATE'], []];
+        yield 'empty-string' => [['NO KEY UPDATE', ''], ''];
+        yield 'string' => [['NO KEY UPDATE', 'UPDATE'], 'UPDATE'];
+        yield 'list' => [['NO KEY UPDATE', 'UPDATE', 'SHARE'], ['UPDATE', 'SHARE']];
+    }
+
+    #[DataProvider('dataAddFor')]
+    public function testAddFor(array $expected, array|string|null $value): void
+    {
+        $db = $this->getConnection();
+
+        $query = (new Query($db))->for('NO KEY UPDATE')->addFor($value);
+
+        assertSame($expected, $query->getFor());
+    }
+
+    public static function dataAddForOnly(): iterable
+    {
+        yield 'null' => [[], null];
+        yield 'empty-list' => [[], []];
+        yield 'empty-string' => [[''], ''];
+        yield 'string' => [['UPDATE'], 'UPDATE'];
+        yield 'list' => [['UPDATE', 'SHARE'], ['UPDATE', 'SHARE']];
+    }
+
+    #[DataProvider('dataAddForOnly')]
+    public function testAddForOnly(array $expected, array|string|null $value): void
+    {
+        $db = $this->getConnection();
+
+        $query = (new Query($db))->addFor($value);
+
+        assertSame($expected, $query->getFor());
+    }
+
+    public static function dataSetFor(): iterable
+    {
+        yield 'null' => [[], null];
+        yield 'empty-list' => [[], []];
+        yield 'empty-string' => [[''], ''];
+        yield 'string' => [['UPDATE'], 'UPDATE'];
+        yield 'list' => [['UPDATE', 'SHARE'], ['UPDATE', 'SHARE']];
+    }
+
+    #[DataProvider('dataSetFor')]
+    public function testSetFor(array $expected, array|string|null $value): void
+    {
+        $db = $this->getConnection();
+
+        $query = (new Query($db))->for('NO KEY UPDATE')->setFor($value);
+
+        assertSame($expected, $query->getFor());
+    }
+
+    public static function dataSetForOnly(): iterable
+    {
+        yield 'null' => [[], null];
+        yield 'empty-list' => [[], []];
+        yield 'empty-string' => [[''], ''];
+        yield 'string' => [['UPDATE'], 'UPDATE'];
+        yield 'list' => [['UPDATE', 'SHARE'], ['UPDATE', 'SHARE']];
+    }
+
+    #[DataProvider('dataSetForOnly')]
+    public function testSetForOnly(array $expected, array|string|null $value): void
+    {
+        $db = $this->getConnection();
+
+        $query = (new Query($db))->setFor($value);
+
+        assertSame($expected, $query->getFor());
     }
 }
