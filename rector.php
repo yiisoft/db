f55010649ce348167a0e1a7f8b8b641f458b6fd2
Sergei Tigrov <rrr-r@ya.ru>
2025-06-30 21:48:06 +0700

    Refactor workflows (#997)

--- rector.php
+++ rector.php
@@ -8,23 +8,17 @@ use Rector\Php74\Rector\Property\RestoreDefaultNullToNullableTypePropertyRector;
 use Rector\Php80\Rector\Ternary\GetDebugTypeRector;
 use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
-use Rector\Set\ValueObject\LevelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
-    ]);
-
-    // register a single rule
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-
-    // define sets of rules
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
-    ]);
-
-    $rectorConfig->skip([
+    ])
+    ->withPhpSets(php81: true)
+    ->withRules([
+        InlineConstructorDefaultToPropertyRector::class,
+    ])
+    ->withSkip([
         RestoreDefaultNullToNullableTypePropertyRector::class => [
             __DIR__ . '/src/Expression/CaseExpression.php',
         ],
@@ -34,4 +28,3 @@ return static function (RectorConfig $rectorConfig): void {
         ReadOnlyPropertyRector::class,
         NullToStrictStringFuncCallArgRector::class,
     ]);
-};
