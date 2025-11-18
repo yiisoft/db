eea4bcbda16b23755dd4269dda9623ebd57821de
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-11-18 15:57:48 +0300

    step 20

--- tests/Db/Expression/Value/StructuredValueTest.php
+++ tests/Db/Expression/Value/StructuredValueTest.php
@@ -12,27 +12,26 @@ use Yiisoft\Db\Query\Query;
 use Yiisoft\Db\Schema\Column\AbstractStructuredColumn;
 use Yiisoft\Db\Schema\Column\ColumnBuilder;
 use Yiisoft\Db\Schema\Data\JsonLazyArray;
-use Yiisoft\Db\Tests\Support\TestTrait;
+use Yiisoft\Db\Tests\Support\TestHelper;
 
 /**
  * @group db
  */
 final class StructuredValueTest extends TestCase
 {
-    use TestTrait;
-
     public static function constructProvider(): array
     {
         $column = ColumnBuilder::structured('currency_money_structured', [
             'value' => ColumnBuilder::money(10, 2),
             'currency' => ColumnBuilder::char(3),
         ]);
+        $db = TestHelper::createSqliteMemoryConnection();
 
         return [
             [[5, 'USD'], null],
             [['value' => 5, 'currency' => 'USD'], null],
             [new ArrayIterator([5, 'USD']), $column],
-            [new Query(self::getDb()), 'currency_money_structured'],
+            [new Query($db), 'currency_money_structured'],
             [new JsonLazyArray('[5,"USD"]'), null],
             [(object) ['value' => 5, 'currency' => 'USD'], null],
         ];
