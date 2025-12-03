f04d1dd2924f514695e26ebfba2cd6e616be9130
Tigrov <rrr-r@ya.ru>
2025-12-03 17:00:58 +0700

    Add source of column information

---
+++ src/Constant/ColumnInfoSource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Yiisoft\Db\Constant;
+
+use Yiisoft\Db\Schema\Column\ColumnFactoryInterface;
+
+/**
+ * Defines the possible sources of column information.
+ *
+ * It can be used in `ColumnFactoryInterface` instances when creating column objects.
+ *
+ * @see ColumnFactoryInterface
+ */
+final class ColumnInfoSource
+{
+    /**
+     * @var string Used to indicate that column information is taken from the column definition.
+     */
+    public const DEFINITION = 'definition';
+
+    /**
+     * @var string Used to indicate that column information is taken from the database table schema.
+     */
+    public const TABLE_SCHEMA = 'table_schema';
+
+    /**
+     * @var string Used to indicate that column information is taken from the query result.
+     */
+    public const QUERY_RESULT = 'query_result';
+}
