0f64f53a515a2b242ad55af054c2c8647524290b
Siarhei Bautrukevich <bautrukevich@users.noreply.github.com>
2026-04-03 08:58:05 +0300

    Update src/Command/AbstractCommand.php
    These annotations are not needed if they are the same as in the interface CommandInterface
    
    Co-authored-by: Sergei Tigrov <rrr-r@ya.ru>

--- src/Command/AbstractCommand.php
+++ src/Command/AbstractCommand.php
@@ -472,31 +472,6 @@ abstract class AbstractCommand implements CommandInterface
         return $this;
     }
 
-    /**
-     * Sets a closure (anonymous function) which is called when a database exception is thrown when executing the command.
-     *
-     * The signature of the closure should be:
-     *
-     * ```php
-     * use Yiisoft\Db\Exception\Exception;
-     * use Yiisoft\Db\Command\CommandInterface;
-     *
-     * function (Exception $e, int $attempt, CommandInterface $command): bool
-     * {
-     *      // return true or false (whether to retry the command or throw $e)
-     * }
-     * ```
-     *
-     * The closure will receive an {@see Exception} converted from the thrown database exception,
-     * the current attempt to execute the command (starting from `0`), and the {@see CommandInterface}
-     * instance to allow access to the command's parameters and other state exposed by the interface.
-     *
-     * If the closure returns `true`, the command will be retried. If the closure returns `false`,
-     * the {@see Exception} will be thrown.
-     *
-     * @param Closure|null $handler A PHP callback to handle database exceptions.
-     * @psalm-param Closure(Exception, int, CommandInterface): bool|null $handler
-     */
     public function setRetryHandler(?Closure $handler): static
     {
         $this->retryHandler = $handler;
